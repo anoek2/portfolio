d9c75a85a8f0ee4a7dbf6985f7084385f913a7db
Anouk van Reen <a.vanreen@hotmail.com>
2019-06-02 15:31:10 +0200

    Portfolio1

---
+++ 1819_IT2_vanReenA_oefening week 11/script.js
@@ -0,0 +1,11 @@
+let menuKnop = document.querySelector(".menu");
+let navMenu = document.querySelector("nav");
+let mainGedeelte = document.querySelector("main");
+function verbergMenu() {
+	navMenu.classList.add("onzichtbaar");
+}
+mainGedeelte.addEventListener("click",verbergMenu);
+function toonMenu() {
+	navMenu.classList.remove("onzichtbaar");
+}
+menuKnop.addEventListener("click",toonMenu);
